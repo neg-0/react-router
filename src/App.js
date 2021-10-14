29bd5c436d3e79300e55db8b751c13a3a1bbd288
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-10-14 15:15:11 -0400

    Added assignment details

--- src/App.js
+++ src/App.js
@@ -23,3 +23,15 @@ function App() {
 }
 
 export default App;
+
+// Create a simple mockup of an ecommerce site (resembling an online store). Don't worry, this is just a mockup, so it doesn't have to have images or a functioning payment system!
+
+// Use React Router to make this a single-page-app that can change its view via the routes (tip - use a navbar along the top with buttons).
+
+// It should feature routes to the following components:
+
+// Home page
+// Products page
+// Individual product page
+// View cart
+// Checkout page
